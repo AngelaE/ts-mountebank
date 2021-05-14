6407a27fa57b2f00f7e7c7de2ba53ae0e67794f9
Angela <angela.koepp@gmail.com>
2021-05-14 21:42:26 +1200

    Add typings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export {Stub} from "./stub";
+export {Stub, DefaultStub} from "./stub";
 export {Response, NotFoundResponse} from "./response";
 export {Predicate, DefaultPredicate, EqualPredicate} from "./predicate";
 export {Imposter, DefaultImposter} from "./imposter";
 export {HttpMethod} from "./http-method";
+export {Mountebank} from './mountebank';
